637f57c3863c4a907a7a1806fc182e1d3b88ba0d
Vitaly <mirenrich@yandex.ru>
2023-10-19 12:35:34 +0300

    log STRAPI_ADMIN_BACKEND_URL

--- config/admin.js
+++ config/admin.js
@@ -1,13 +1,16 @@
-module.exports = ({ env }) => ({
-  auth: {
-    secret: env('ADMIN_JWT_SECRET'),
-  },
-  apiToken: {
-    salt: env('API_TOKEN_SALT'),
-  },
-  transfer: {
-    token: {
-      salt: env('TRANSFER_TOKEN_SALT'),
+module.exports = ({env}) => {
+  console.log('STRAPI_ADMIN_BACKEND_URL', env('STRAPI_ADMIN_BACKEND_URL'));
+  return {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET'),
     },
-  },
-});
+    apiToken: {
+      salt: env('API_TOKEN_SALT'),
+    },
+    transfer: {
+      token: {
+        salt: env('TRANSFER_TOKEN_SALT'),
+      },
+    },
+  }
+};
